343058574bde78844e915b306cebc6673e14d43a
Sangeeth <sarasangeetharajang@gmail.com>
2017-05-11 16:11:42 +0530

    fghjkl

--- src/main/java/org/webapp/Admission.java
+++ src/main/java/org/webapp/Admission.java
@@ -39,7 +39,7 @@ public class Admission extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		// doGet(request, response);
-		System.out.println("Insertion start-------!!");
+		System.out.println("Insertion started...-------!!");
 		String stname=request.getParameter("stname");
 		String rollno=request.getParameter("roll");
 		String course=request.getParameter("course");
